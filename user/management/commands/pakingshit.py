f31b507aafa780f209925c5b2e72295d0694d78b
silverwing47 <90695743+silverwing47@users.noreply.github.com>
2022-12-19 17:41:09 +0800

    sdadsa

--- user/management/commands/pakingshit.py
+++ user/management/commands/pakingshit.py
@@ -7,13 +7,12 @@ class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(email='admin').exists():
-            User.objects.create_superuser(
-                email='admin@gmail.com',
-                password='Pass123.',
-                firstname = 'Angel',
-                lastname = 'Singson',
-                gender = 'Male',
-                phone = '09054592625',
-            )
-        print('Superuser has been created.')
+        User.objects.create_superuser(
+            email='adminrawr@gmail.com',
+            password='Pass123.',
+            firstname = 'Angelll',
+            lastname = 'Singsonss',
+            gender = 'Male',
+            phone = '09054592625',
+        )
+        
