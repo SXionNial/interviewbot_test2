50bff2d37a77c93f156f15ffae1cf42b62502876
silverwing47 <90695743+silverwing47@users.noreply.github.com>
2022-12-19 13:28:41 +0800

    dsad

--- InterviewBot/settings.py
+++ InterviewBot/settings.py
@@ -45,8 +45,8 @@ DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 TEMPLATE_DEBUG = DEBUG
 
 #ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
